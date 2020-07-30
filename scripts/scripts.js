9f43bed9d65d2ac077b51a137d103580fb9a8883
Ryan Leahy <rpleahy12@gmail.com>
2020-07-30 14:28:00 -0700

    Added section highlighting and fixed weird jump when nav bar snapped to top
    Also declared the encoding type and language type.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,32 @@
-function isElementInViewport (el) 
+function isElementInViewport(el, heightParam) 
 {
-    var rect = el[0].getBoundingClientRect();
-    console.log(el[0].clientHeight)
+    var rect = el.getBoundingClientRect();
     return (
-        rect.top >= -el[0].clientHeight &&
+        rect.top >= heightParam &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
+function isElementPartiallyInViewport(el, heightParam)
+{
+    
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.bottom >= heightParam && 
+        rect.top < (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
 function changeNavPos()
 {
-    var titlebarOnScreen = isElementInViewport(document.getElementsByClassName("titlebar"));
+    var titlebar = document.getElementsByClassName("titlebar")[0];
+    var titlebarOnScreen = isElementInViewport(titlebar, -titlebar.clientHeight);
+
     var navElement;
+    
+    var mainContainer = document.getElementsByClassName("maincontainer")[0];
 
     if(titlebarOnScreen)
     {
@@ -23,6 +36,7 @@ function changeNavPos()
         {
             navElement.classList.remove("navfixed");
             navElement.classList.add("nav")
+            mainContainer.style.top = "35px";
         }   
     }
     else
@@ -33,10 +47,44 @@ function changeNavPos()
         {
             navElement.classList.remove("nav");
             navElement.classList.add("navfixed")
+            mainContainer.style.top = "70px";
         }   
     }
 }
 
-function test()
+function onScroll()
 {
+    changeNavPos();
+    highlightSectionLink();
+}
+
+function highlightSectionLink()
+{
+    var titlebar = document.getElementsByClassName("titlebar")[0]
+    var titlebarOnScreen = isElementInViewport(titlebar, -titlebar.clientHeight);
+
+    var aboutmecontainer = document.getElementById("aboutmecontainer")
+    var aboutmeOnScreen = isElementPartiallyInViewport(aboutmecontainer, 35);
+
+    var aboutmelink = document.getElementsByClassName("navlinks")[0];
+    var portfoliolink = document.getElementsByClassName("navlinks")[1];
+
+    if(titlebarOnScreen)
+    {
+        aboutmelink.id = "";
+        portfoliolink.id = "";
+    }
+    else
+    {
+        if(aboutmeOnScreen)
+        {
+            aboutmelink.id = "aboutmelink";
+            portfoliolink.id = "";
+        }
+        else
+        {
+            aboutmelink.id = "";
+            portfoliolink.id = "portfoliolink";
+        }
+    }
 }

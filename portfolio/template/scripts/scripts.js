c2574b44b7fb773c36d12efeae7d00f85f94bb3a
Ryan Leahy <rpleahy12@gmail.com>
2020-08-03 19:43:14 -0700

    Created a template for projects
    I wanted to create a template folder of assets to use to make websites showing off my project work. Once I have a new project all I have to do is copy the template folder and edit the page accordingly.

--- portfolio/template/scripts/scripts.js
+++ portfolio/template/scripts/scripts.js
@@ -44,4 +44,37 @@ function changeNavPos()
 function onScroll()
 {
     changeNavPos(); //checks if the nav bar should be snapped to the top of the screen
+    highlightSectionLink(); //checks which section is currently in the viewport to highlight the corresponding link in the nav bar
+}
+
+//function handles highlighting the link for the section currently being displayed in the viewport
+function highlightSectionLink()
+{
+    var titlebar = document.getElementById("titlebar") //need to know if titlebar is on screen
+    var titlebarOnScreen = isElementPartiallyInViewport(titlebar, 0);
+
+    var aboutmecontainer = document.getElementById("aboutmecontainer") //need to know if aboutmecontainer is still on screen
+    var aboutmeOnScreen = isElementPartiallyInViewport(aboutmecontainer, 35);
+
+    var aboutmelink = document.getElementsByClassName("navlinks")[0]; //get the nav links that may be highlighted
+    var portfoliolink = document.getElementsByClassName("navlinks")[1];
+
+    if(titlebarOnScreen) //if the titlebar is on screen neither links should be highlighted
+    {
+        aboutmelink.id = "";
+        portfoliolink.id = "";
+    }
+    else //if you get here it means the titlebar is not on the screen and we can check what section we're looking at
+    {
+        if(aboutmeOnScreen) //if the about me section is on screen 
+        {
+            aboutmelink.id = "aboutmelink"; //apply the id to the about me link that will highlight it
+            portfoliolink.id = "";
+        }
+        else //if here that means we're looking at the portfolio
+        {
+            aboutmelink.id = "";
+            portfoliolink.id = "portfoliolink"; //so highlight the portfolio by applying an id
+        }
+    }
 }
